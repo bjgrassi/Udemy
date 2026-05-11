3e3dcc70bc249387ff7096a9ec16e21796b255db
Bruna <brukgrassi@gmail.com>
2026-05-11 19:53:40 -0400

    Section 4: [Optional] Review of Essential JavaScript for React

--- ultimate-react-course/02-JS-review/script.js
+++ ultimate-react-course/02-JS-review/script.js
@@ -144,19 +144,168 @@ function getBook(id) {
 }
 
 // Destructuring
-const book = getBook(3);
-book;
+const book1 = getBook(3);
+book1;
 
-// const title = book.title;
-// const author = book.author;
+// const title = book1.title;
+// const author = book1.author;
 
 const { title, author, pages, publicationDate, genres, hasMovieAdaptation } =
-  book;
+  book1;
 
 console.log(author, title, genres);
 
 // const primaryGenre = genres[0];
 // const secondaryGenre = genres[1];
 
-const [primaryGenre, secondaryGenre] = genres;
-console.log(primaryGenre, secondaryGenre);
+// 4.19 - Rest operator '...otherGenres'
+const [primaryGenre, secondaryGenre, ...otherGenres] = genres;
+console.log(primaryGenre, secondaryGenre, otherGenres);
+
+// 4.19 - Spread operator '...genres'
+const newGenres = ["epic fantasy", ...genres];
+newGenres;
+
+const updatedBook = {
+  ...book1, // spread operator to copy all properties of 'book1'. It must be the first one
+
+  moviePublicationDate: "2001-12-19", // Adding a new property
+  pages: 1210, // override 'pages' property
+};
+updatedBook;
+
+// 4.22 - Arrow functions
+// function getYear(str) {
+//   return str.split("-")[0];
+// }
+
+const getYear = (str) => str.split("-")[0];
+console.log(getYear(publicationDate));
+
+// 4.20 - Template literals
+const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${getYear(
+  publicationDate,
+)}. The book has ${hasMovieAdaptation ? "" : "not"} been adapted as a movie`;
+summary;
+
+// 4.21 - Ternary operator
+const pagesRange = pages > 1000 ? "over a thousand" : "less than 1000";
+pagesRange;
+console.log(`The book has ${pagesRange} pages`);
+
+// 4.23 - Short-circuiting and Logical operators: &&, ||, ??
+console.log(true && "Some string");
+console.log(false && "Some string");
+console.log(hasMovieAdaptation && "This book has a movie");
+
+// falsy: 0, '', null, undefined
+console.log("jonas" && "Some string");
+console.log(0 && "Some string");
+
+console.log(true || "Some string");
+console.log(false || "Some string");
+
+console.log(book1.translations.spanish);
+
+const spanishTranslation = book1.translations.spanish || "NOT TRANSLATED";
+spanishTranslation;
+
+console.log(book1.reviews.librarything?.reviewsCount);
+const countWrong = book1.reviews.librarything?.reviewsCount || "no data";
+countWrong;
+
+// nullish coalescing operator. It only considers null and undefined as falsy values, not 0 or ''.
+const count = book1.reviews.librarything?.reviewsCount ?? "no data";
+count;
+
+// 4.24 - Optional chaining operator '?.'
+function getTotalReviewCount(book1) {
+  const goodreads = book1.reviews?.goodreads?.reviewsCount;
+  const librarything = book1.reviews?.librarything?.reviewsCount ?? 0;
+  librarything;
+  return goodreads + librarything;
+}
+
+console.log(getTotalReviewCount(book1));
+
+// 4.25 - The array map() method -> functional array methods --these methods do not mutate the original array
+const books = getBooks();
+books;
+
+const x = [1, 2, 3, 4, 5].map((el) => el * 2);
+console.log(x);
+
+const titles = books.map((book) => book.title);
+titles;
+
+const essentialData = books.map((book) => ({
+  title: book.title,
+  author: book.author,
+  reviewsCount: getTotalReviewCount(book),
+}));
+essentialData;
+
+// 4.26 - The array filter() method -> functional array methods --these methods do not mutate the original array
+const longBooksWithMovie = books
+  .filter((book) => book.pages > 500)
+  .filter((book) => book.hasMovieAdaptation);
+longBooksWithMovie;
+
+const adventureBooks = books
+  .filter((book) => book.genres.includes("adventure"))
+  .map((book) => book.title);
+adventureBooks;
+
+// 4.27 - The array reduce() method -> functional array methods --these methods do not mutate the original array
+const pagesAllBooks = books.reduce((sum, book) => sum + book.pages, 0);
+pagesAllBooks;
+
+// 4.28 - The array sort() method -> use slice() to create a copy of the original array, because sort() mutates the original array
+const arr = [3, 7, 1, 9, 6];
+const sorted = arr.slice().sort((a, b) => a - b); // Ascending order
+sorted;
+arr;
+
+const sortedByPages = books.slice().sort((a, b) => b.pages - a.pages); // Descending order
+sortedByPages;
+
+// 4.29 - Working with immutable arrays
+// 1) Add book object to array
+const newBook = {
+  id: 6,
+  title: "Harry Potter and the Chamber of Secrets",
+  author: "J. K. Rowling",
+};
+const booksAfterAdd = [...books, newBook];
+booksAfterAdd;
+
+// 2) Delete book object from array
+const booksAfterDelete = booksAfterAdd.filter((book) => book.id !== 3);
+booksAfterDelete;
+
+// 3) Update book object in array
+const booksAfterUpdate = booksAfterDelete.map((book) =>
+  book.id === 2 ? { ...book, pages: 350 } : book,
+);
+booksAfterUpdate;
+
+// 4.30 - Asynchronous JavaScript: Promises
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+
+console.log("jonas");
+
+// 4.31 - Asynchronous JavaScript: Async/Await
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+
+const todos = getTodos();
+console.log(todos);
+
+console.log("jonas");
--- ultimate-react-course/README.md
+++ ultimate-react-course/README.md
@@ -1,1 +1,1 @@
-Courses
+Instructor's github project https://github.com/jonasschmedtmann/ultimate-react-course/tree/main
