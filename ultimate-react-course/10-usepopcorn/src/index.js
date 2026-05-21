d6aa1ff2e9b584c41bf6b6b5629d558696eb3b90
Bruna <brukgrassi@gmail.com>
2026-05-20 20:18:07 -0400

    [FINISHED] Section 10: Thinking in React: Components, Composition, and Reusability

--- ultimate-react-course/10-usepopcorn/src/index.js
+++ ultimate-react-course/10-usepopcorn/src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+// import StarRating from "./StarRating";
+import App from "./Challenge10";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    {/* <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={2} />
+
+    <Test /> */}
   </React.StrictMode>,
 );
