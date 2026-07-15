0d5c10b69f7aa7523691242315b27627e0e8d39e
Bruna <brukgrassi@gmail.com>
2026-07-15 09:18:30 -0400

    [FINISHED] Section 24: Adding Redux and Advanced React Router

--- ultimate-react-course/22-fast-react-pizza/src/App.jsx
+++ ultimate-react-course/22-fast-react-pizza/src/App.jsx
@@ -1,14 +1,16 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home";
+import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder, {
   action as createOrderAction,
 } from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
+
 import AppLayout from "./ui/AppLayout";
-import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
@@ -26,10 +28,7 @@ const router = createBrowserRouter([
         loader: menuLoader,
         errorElement: <Error />,
       },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
+      { path: "/cart", element: <Cart /> },
       {
         path: "/order/new",
         element: <CreateOrder />,
@@ -40,6 +39,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
