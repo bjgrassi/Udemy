ebca0f952bea1e7d50858b2a4c2ebeb7a4842a7c
Bruna <brukgrassi@gmail.com>
2026-07-20 19:23:14 -0400

    [FINISHED] Section 27: React Query: Managing Remote State

--- ultimate-react-course/25-the-wild-oasis/src/services/supabase.js
+++ ultimate-react-course/25-the-wild-oasis/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://jlpewvkklcjvlmzrleqe.supabase.co";
+export const supabaseUrl = "https://jlpewvkklcjvlmzrleqe.supabase.co";
 const supabaseKey = "sb_publishable_Fl3sfS4gIKsFLRCEqixrFg_-tsEX4Ix";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
